72041beddb42a8c7a1f4081ebdf6f75a88034937
o-kovalenko01 <kovalenkoleg777@gmail.com>
2023-10-29 13:57:16 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,25 @@
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
 
+let intervalId = null;
 
+startButton.addEventListener('click', () => {
+  if (intervalId === null) {
+    intervalId = setInterval(() => {
+      document.body.style.backgroundColor = getRandomHexColor();
+    }, 850);
+    startButton.disabled = true;
+  }
+});
 
+stopButton.addEventListener('click', () => {
+  clearInterval(intervalId);
+  intervalId = null;
+  startButton.disabled = false;
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
+}
